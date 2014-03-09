02fe4e7909a299a9f4e9dcbbd2db2af5a95deb8a
Dawn <wangsguang@gmail.com>
2014-03-09 10:59:34 +0800

    create idly

---
+++ IFly_Test/IFly_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IFly_Test
+//
+//  Created by Dawn on 14-3-9.
+//  Copyright (c) 2014年 Dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IFly_Test/IFly_TestTests/IFly_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  IFly_TestTests.m
+//  IFly_TestTests
+//
+//  Created by Dawn on 14-3-9.
+//  Copyright (c) 2014年 Dawn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IFly_TestTests : XCTestCase
+
+@end
+
+@implementation IFly_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
